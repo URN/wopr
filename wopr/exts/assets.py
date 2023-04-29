52a3010e1ef380bc3252af1d95ab1a9c7a6b4e58
Mark Fannon <markjfannon@Markbook-Pro.local>
2023-04-30 00:52:32 +0100

    Implemented asset ingestion command

--- wopr/exts/assets.py
+++ wopr/exts/assets.py
@@ -1,5 +1,6 @@
 """Utilities for importing assets."""
-import tempfile
+import os
+import shutil
 
 import discord
 import requests
@@ -14,19 +15,34 @@ class Assets(commands.GroupCog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
+    async def download_autocomplete(
+        self,
+        interaction: discord.Interaction,
+        current: str,
+    ) -> list[app_commands.Choice[str]]:
+        """Offer an autofilled response to download call."""
+        choices = ["Beds", "Songs", "Idents"]
+        return [app_commands.Choice(name=choice, value=choice) for choice in choices]
+
     @app_commands.command()
-    async def linkcheck(
+    @app_commands.autocomplete(type=download_autocomplete)
+    async def download(
         self,
         interaction: discord.Interaction,
         attachment: str,
+        type: str,
     ) -> None:
         """Download an attachment and adds it to Zetta mount."""
-        temp = tempfile.TemporaryFile()
-
         if attachment.startswith("https://cdn.discordapp.com"):
-            temp.write(requests.get(attachment, timeout=5).content)
-            temp.close()
-            await interaction.response.send_message("This is a correct link")
+            if requests.get(attachment, timeout=20).headers["content-type"] == "audio/mpeg":
+                local_filename = attachment.split("/")[-1].replace("_", " ")
+                path = os.path.join(f"/mnt/Imports/{type}/{local_filename}")
+                with requests.get(attachment, stream=True, timeout=25) as r, open(path, "wb") as f:
+                    shutil.copyfileobj(r.raw, f)
+                f.close()
+                await interaction.response.send_message(":white_check_mark: Downloaded file")
+            else:
+                await interaction.response.send_message(":x: This is not an MP3 file!")
 
         else:
             await interaction.response.send_message(":x: Please submit a Discord attachment link")
