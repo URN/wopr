ba40445dac3eca62b8a701a08c340ae2536f5b5a
Mark Fannon <markjfannon@Markbook-Pro.local>
2023-05-10 16:50:04 +0100

    Actioned requested changes

--- wopr/exts/assets.py
+++ wopr/exts/assets.py
@@ -1,9 +1,9 @@
 """Utilities for importing assets."""
 import os
-import shutil
+from typing import Literal
 
+import aiohttp
 import discord
-import requests
 from discord import app_commands
 from discord.ext import commands
 
@@ -15,37 +15,33 @@ class Assets(commands.GroupCog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
-    async def download_autocomplete(
-        self,
-        interaction: discord.Interaction,
-        current: str,
-    ) -> list[app_commands.Choice[str]]:
-        """Offer an autofilled response to download call."""
-        choices = ["Beds", "Songs", "Idents"]
-        return [app_commands.Choice(name=choice, value=choice) for choice in choices]
-
     @app_commands.command()
-    @app_commands.autocomplete(type=download_autocomplete)
     async def download(
         self,
         interaction: discord.Interaction,
+        type: Literal["Idents", "Songs", "Beds"],
         attachment: str,
-        type: str,
     ) -> None:
         """Download an attachment and adds it to Zetta mount."""
-        if attachment.startswith("https://cdn.discordapp.com"):
-            if requests.get(attachment, timeout=20).headers["content-type"] == "audio/mpeg":
-                local_filename = attachment.split("/")[-1].replace("_", " ")
-                path = os.path.join(f"/mnt/Imports/{type}/{local_filename}")
-                with requests.get(attachment, stream=True, timeout=25) as r, open(path, "wb") as f:
-                    shutil.copyfileobj(r.raw, f)
-                f.close()
-                await interaction.response.send_message(":white_check_mark: Downloaded file")
-            else:
-                await interaction.response.send_message(":x: This is not an MP3 file!")
-
-        else:
+        if not attachment.startswith("https://cdn.discordapp.com"):
             await interaction.response.send_message(":x: Please submit a Discord attachment link")
+            return
+
+        async with aiohttp.ClientSession() as session:
+            async with session.head(attachment) as resp:
+                if resp.headers["content-type"] != "audio/mpeg":
+                    await interaction.response.send_message(":x: This is not an MP3 file!")
+                    return
+
+            local_filename = attachment.split("/")[-1].replace("_", " ")
+            path = os.path.join(f"/mnt/Imports/{type}/{local_filename}")
+
+            async with session.get(attachment) as resp:
+                with open(path, "wb") as f:
+                    async for chunk in resp.content.iter_chunked():
+                        f.write(chunk)
+
+        await interaction.response.send_message(":white_check_mark: Downloaded file")
 
 
 async def setup(bot: commands.Bot) -> None:
